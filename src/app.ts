40b22b1fe6a7a9cc0e23ecb590778d338213c8f7
bela016 <isabela.seixas2014@gmail.com>
2024-02-02 16:12:36 -0300

    challenge #2 02-02-2024

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import express  from 'express';
-import cors  from 'cors';
+import express from 'express';
+import cors from 'cors';
+import { Ave } from './model/Ave';
+import { Reptil } from './model/Reptil';
+import { Mamifero } from './model/Mamifero';
 
 const port = 3000;
 const server = express();
@@ -8,9 +11,20 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-    res.json('olá mundo!');
+    let ave: Ave = new Ave('Papagaio', 30, 'masculino', 12);
+    let reptil: Reptil = new Reptil('lagarto', 2, 'femea', 'cicloides');
+    let mamifero: Mamifero = new Mamifero('doberman', 'cachorro', 102, 'femea');
+    res.json([ave, reptil, mamifero]);
+    //res.json('olá mundo!');
 })
+
 
 server.listen(port, () => {
     console.log(`servidor está escutando no endereço http://localhost:${port}`)
+})
+
+server.post('/Aves', (req, res) => {
+    const { nome, idade, genero, envergadura } = req.body;
+    const NvAve = new Ave(nome, idade, genero, envergadura);
+    res.json(NvAve);
 })
--- src/model/Reptil.ts
+++ src/model/Reptil.ts
@@ -1,10 +1,13 @@
-class Reptil extends Animal {
+import { Animal } from "./Animal";
+
+export class Reptil extends Animal {
     private tipo_escama: string;
 
-    constructor(_tipo_escama: string,
+    constructor(
         _nome: string,
         _idade: number,
-        _genero: string) {
+        _genero: string,
+        _tipo_escama: string) {
         super(_nome, _idade, _genero);
         this.tipo_escama = _tipo_escama;
     }
