373115243e6fee40e34394b58edfe81198c0b0bc
bela016 <isabela.seixas2014@gmail.com>
2024-02-28 13:27:41 -0300

    app,init,mamifero,habitat e ave atualizados

--- docs/init.sql
+++ docs/init.sql
@@ -1,41 +1,93 @@
-CREATE TABLE mamifero (id SERIAL NOT NULL PRIMARY KEY ,
-					 nome VARCHAR (50) NOT NULL, 
-					 idade INT,
-					 genero VARCHAR(12)NOT NULL, 
-					 raca VARCHAR (20)  );
+------------------------------------------------------------------------------------
+------------- CRIANDO TABELAS
+------------------------------------------------------------------------------------ 
+CREATE TABLE Animal (idAnimal SERIAL NOT NULL PRIMARY KEY,
+					nomeAnimal VARCHAR(50) NOT NULL,
+					idadeAnimal INT,
+					generoAnimal VARCHAR(15) NOT NULL);
+					
+CREATE TABLE Ave (idAve INT,
+				 envergadura FLOAT NOT NULL,
+				 FOREIGN KEY (idAve) REFERENCES Animal(idAnimal));
+				 
+CREATE TABLE Mamifero (idMamifero INT,
+				 especie VARCHAR(50) NOT NULL,
+				 FOREIGN KEY (idMamifero) REFERENCES Animal(idAnimal));
+				 
+CREATE TABLE Reptil (idReptil INT,
+				 tipoDeEscamas VARCHAR(50) NOT NULL,
+				 FOREIGN KEY (idReptil) REFERENCES Animal(idAnimal));
+				 
+CREATE TABLE Habitat (idHabitat SERIAL NOT NULL PRIMARY KEY,
+					 nomeHabitat VARCHAR(50) NOT NULL);
 					 
-CREATE TABLE repitil(id SERIAL NOT NULL PRIMARY KEY ,
-					 nome VARCHAR(50) NOT NULL, 
-					 idade INT,
-					 genero VARCHAR(12)NOT NULL, 
-					 tipo_de_escamas VARCHAR(50));
+CREATE TABLE Atracao (idAtracao SERIAL NOT NULL PRIMARY KEY,
+					 nomeAtracao VARCHAR(50) NOT NULL,
+					 idHabitat INT,
+					 FOREIGN KEY (idHabitat) REFERENCES Habitat(idHabitat));
 					 
-CREATE TABLE ave (id SERIAL NOT NULL PRIMARY KEY ,
-					 nome VARCHAR (50) NOT NULL, 
-					 idade INT,
-					 genero VARCHAR(12)NOT NULL, 
-					 envergadura FLOAT);
-					 
-INSERT INTO mamifero (nome, idade, genero, raca)
-	VALUES
-('simba',2,'macho','leão'),
-('timao',1,'macho','suricato'),
-('pumba',10,'macho','javali');
+CREATE TABLE Animal_Habitat(idAnimalHabitat SERIAL NOT NULL PRIMARY KEY,
+						   idAnimal INT,
+						   idHabitat INT,
+						   FOREIGN KEY (idAnimal) REFERENCES Animal(idAnimal),
+						   FOREIGN KEY (idHabitat) REFERENCES Habitat(idHabitat));
 
-INSERT INTO repitil (nome, idade, genero, tipo_de_escamas)
+------------------------------------------------------------------------------------
+------------- POPULANDO TABELAS (INSERT)
+------------------------------------------------------------------------------------
+INSERT INTO Animal(nomeAnimal, idadeAnimal, generoAnimal)
 	VALUES
-('zazu',2,'macho','dermica'),
-('crush',1,'macho','escudos'),
-('kaa',10,'macho','quiliferas');
-
-INSERT INTO ave (nome, idade, genero, envergadura)
+	('SIMBA', 7, 'MACHO'),			-- id 1
+	('PUMBA', 4, 'MACHO'),			-- id 2
+	('NALA', 6, 'FÊMEA'),			-- id 3
+	('ZAZU', 1, 'MACHO'),			-- id 4
+	('IAGO', 2, 'MACHO'),			-- id 5
+	('SCUTTLE', 4, 'FÊMEA'),		-- id 6
+	('KAA', 1, 'MACHO'),			-- id 7
+	('LOUIS', 5, 'MACHO'),			-- id 8
+	('TICK-TOCK', 3, 'FÊMEA');		-- id 9
+	
+INSERT INTO Mamifero(idMamifero, especie)
 	VALUES
-('iago',2,'macho', 20),
-('scuttle',1,'macho',40);
+	(1, 'FELINO'),
+	(2, 'JAVALI'),
+	(3, 'FELINO');
 
- --SELECT * FROM mamifero;
- --SELECT * FROM ave;
- --SELECT * FROM repitil;
- 
- --ALTER TABLE repitil
- --ALTER COLUMN tipo_de_escamas TYPE VARCHAR (50);
+INSERT INTO Ave(idAve, envergadura)
+	VALUES
+	(4, 0.55),
+	(5, 0.98),
+	(6, 0.35);
+
+INSERT INTO Reptil(idReptil, tipoDeEscamas)
+	VALUES
+	(7, 'ESCUDOS'),
+	(8, 'ESCAMAS CEFÁLICAS'),
+	(9, 'ESCAMAS TIMPANOAURICULARES');
+	
+INSERT INTO Habitat(nomeHabitat)
+	VALUES
+	('PLANÍCIE'),	-- id 1
+	('FLORESTA'),	-- id 2
+	('PÂNTANO'),	-- id 3
+	('MONTANHA'),	-- id 4
+	('ILHA');		-- id 5
+	
+INSERT INTO Animal_Habitat(idAnimal, idHabitat)
+	VALUES
+	(1, 1),
+	(2, 1),
+	(3, 1),
+	(4, 1),
+	(5, 2),
+	(6, 2),
+	(7, 2),
+	(8, 3),
+	(9, 3);
+	
+INSERT INTO Atracao(nomeAtracao, idHabitat)
+	VALUES
+	('PASSEIO DE JEEP', 1),		-- id 1
+	('ALIMENTAR OS LEÕES', 1),	-- id 2
+	('PENTEAR MACACO', 2),		-- id 3
+	('CORRER DAS COBRAS', 3);	-- id 4
